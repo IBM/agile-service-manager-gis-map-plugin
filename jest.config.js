8aa0c3f1541b9e25f9a794a4b6f56b84f9e93d6d
Kilian Collender <kiliancol@uk.ibm.com>
2020-06-18 10:17:32 +0100

    config tests

--- jest.config.js
+++ jest.config.js
@@ -15,7 +15,7 @@ module.exports = {
   clearMocks: true,
 
   // Indicates whether the coverage information should be collected while executing the test
-  // collectCoverage: false,
+  collectCoverage: true,
 
   // An array of glob patterns indicating a set of files for which coverage information should be collected
   collectCoverageFrom: [
@@ -23,7 +23,7 @@ module.exports = {
   ],
 
   // The directory where Jest should output its coverage files
-  coverageDirectory: "coverage",
+  coverageDirectory: '<rootDir>/test/coverage',
 
   // An array of regexp pattern strings used to skip coverage collection
   coveragePathIgnorePatterns: [
--- test/server/config.test.js
+++ test/server/config.test.js
@@ -6,6 +6,16 @@ jest.mock('fs');
 
 
 describe('config', function() {
+    test('readConfig: use defaults if YAML file does not exist', function() {
+        // Mock the fs.readFileSync function so it throws an error
+        fs.existsSync.mockImplementation(() => false);
+
+        config.readConfig();
+        let testSettings = config.settings();
+
+        expect(testSettings).toMatchSnapshot();
+    });
+
     test('readConfig: read config from YAML file', function() {
         // Mock the fs.readFileSync function so it does not actually try to read from a file
         // but instead returns a hardcoded string
@@ -27,4 +37,27 @@ describe('config', function() {
 
         expect(testSettings).toMatchSnapshot();
     });
+
+    test('readConfig: read config from environment variables', function() {
+        process.env.UI_SERVER_PORT = 4000;
+        process.env.PS_HOST = 'host.com';
+
+        config.readConfig();
+        let testSettings = config.settings();
+
+        let expected = 4000;
+        let actual = testSettings.webServerPort;
+        expect(actual).toBe(expected);
+
+        expected = 'host.com';
+        actual = testSettings.proxyServiceHost;
+        expect(actual).toBe(expected);
+
+
+        // Clean up
+        delete process.env.UI_SERVER_PORT;
+        delete process.env.PS_HOST;
+    });
+
+    
 });
